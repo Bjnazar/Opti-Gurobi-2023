03b4060b0d921b935ff11f8fb6ea20d3db042e17
Juanpa Barías <jpl.barias@uc.cl>
2023-05-16 12:45:46 -0400

    idea para definir var Z + fo + par de restr

--- gurobi_e2.py
+++ gurobi_e2.py
@@ -1,5 +1,6 @@
 from gurobipy import GRB, Model, quicksum
 from random import randint, seed
+import math
 
 seed(10)
 
@@ -18,7 +19,7 @@ Destinos = range(num_destinos) # d in D
 Origenes = range(num_origenes) # o in O
 Dias = range(num_dias) # t in T
 Bloques = range(48) # b in {1,...,48}
-Pedidos = lambda p_d: range(p_d) # j in {1,...,pd}, no creo q este bien, solo una idea
+Pedidos = lambda d: range(p[d]) # j in {1,...,pd}, no se si este bien, solo una idea
 
 # Construcción de los parametros
 V = { i : randint(10, 100) for i in Camiones } # V_i
@@ -48,20 +49,80 @@ U = model.addVars(Bloques, Dias, vtype=GRB.INTEGER, name="U_bt")
 X = model.addVars(Camiones, Bloques, Dias, Destinos, vtype=GRB.INTEGER, name="X_ibtd")
 W = model.addVars(Camiones, Bloques, Dias, Origenes, vtype=GRB.INTEGER, name="W_ibto")
 Y = model.addVars(Camiones, Bloques, Dias, Origenes, vtype=GRB.BINARY, name="Y_ibto")
-Z = model.addVars(Camiones, Bloques, Dias, Pedidos(d), Destinos, vtype=GRB.BINARY, name="Z_ibtjd") # no creo q este bien, solo una idea
-# falta alpha_ibt
-# falta beta_i
+# no se si este bien, solo una idea
+variables_z = []
+for d in Destinos:
+  z = model.addVars(Camiones, Bloques, Dias, Pedidos(d), vtype=GRB.BINARY, name=f"Z_ibtj{d}") 
+  variables_z.append(z)
+Z = { 
+  (i,b,t,j,d) : variables_z[d][i,b,t,j] for i in Camiones for b in Bloques for t in Dias for d in Destinos for j in Pedidos(d)
+}
+alpha = model.addVars(Camiones, Bloques, Dias, vtype=GRB.BINARY, name="alpha_ibt")
+beta = model.addVars(Camiones, vtype=GRB.BINARY, name="beta_i")
 
 model.update()
 
 # ------------ Agregar restricciones ------------
+# R1
+b_p = lambda o,i: math.ceil(Do[o]/(2*V[i]))
+model.addConstrs(
+  ( Y[i,b,t,o] == Y[i,b-b_p(o,i),t,o] for i in Camiones for o in Origenes for b in Bloques[b_p(o,i):] for t in Dias), 
+  name="R1a"
+)
+model.addConstrs(
+  ( b_p(o,i) <= 47 for i in Camiones for o in Origenes), 
+  name="R1b"
+)
+
+# R2
+b_pp = lambda d,i: math.ceil(Dd[d]/(2*V[i]))
+model.addConstrs(
+  ( Z[i,b,t,j,d] >= alpha[i,b-b_pp(d,i),t] for i in Camiones for d in Destinos for b in Bloques[b_pp(d,i):] for t in Dias for j in Pedidos(d)), 
+  name="R2a"
+)
+model.addConstrs(
+  ( b_pp(d,i) <= 47 for i in Camiones for d in Destinos), 
+  name="R2b"
+)
+
+# R3
+
+# R4
 model.addConstrs(
   ( U[48,t-1] == U[1,t] for t in Dias[1:]), name="R4"
 )
+
+# R5
+
+# R6
+
+# R7
+
+# R8
+
+# R9
+model.addConstrs(
+  ( U[b,t] <= Qmax for b in Bloques for t in Dias), name="R9"
+)
+
+# R10
+
+# R11
+
+# R12
+
+# R13
+
+# R14 
+# Las restricciones de la naturaleza de las variables las establece gurobi
+#  al crear las variables y definir sus respectivos tipos de datos 
+
 # faltan el resto de las restricciones
 
 # ------------ Función objetivo ------------
-objetivo = quicksum( 2*E[i]*(quicksum( ) + quicksum()) for t in Dias for b in Bloques for i in range(num_camiones_diesel)) #**
+fo_sum1 = lambda t,b,i: quicksum(Z[i,b,t,j,d]*Dd[d] for d in Destinos for j in Pedidos(d))
+fo_sum2 = lambda t,b,i: quicksum(Y[i,b,t,o]*Do[o] for o in Origenes)
+objetivo = quicksum(2*E[i]*( fo_sum1(t,b,i) + fo_sum2(t,b,i) ) for t in Dias for b in Bloques for i in range(num_camiones_diesel))
 model.setObjective(objetivo, GRB.MINIMIZE)
 
 # ------------ Optimización del modelo ------------
