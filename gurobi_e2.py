f9a1f523f1efaae839d84df0dfc296d4639065f7
Juanpa Barías <jpl.barias@uc.cl>
2023-05-17 14:08:13 -0400

    código (casi) completo v1, falta R12c y R12d

--- gurobi_e2.py
+++ gurobi_e2.py
@@ -12,6 +12,7 @@ num_camiones_diesel = 10
 num_destinos = 10
 num_origenes = 10
 num_dias = 10
+bigM = 10**6
 
 # Construcción de los conjuntos
 Camiones = range(num_camiones_diesel + num_camiones_electricos) # i in I
@@ -39,6 +40,7 @@ Cq = randint(10, 100)
 Qmax = randint(10, 100)
 Ce = randint(10, 100)
 G = randint(10, 100)
+R = { o : randint(10, 100) for o in Origenes } # R_o
 
 # ------------ Generar el modelo ------------
 model = Model("Entrega 2 Proyecto")
@@ -48,6 +50,7 @@ model.setParam("TimeLimit", 60)
 U = model.addVars(Bloques, Dias, vtype=GRB.INTEGER, name="U_bt")
 X = model.addVars(Camiones, Bloques, Dias, Destinos, vtype=GRB.INTEGER, name="X_ibtd")
 W = model.addVars(Camiones, Bloques, Dias, Origenes, vtype=GRB.INTEGER, name="W_ibto")
+M = model.addVars(Bloques, Dias, Origenes, vtype=GRB.INTEGER, name="M_bto")
 Y = model.addVars(Camiones, Bloques, Dias, Origenes, vtype=GRB.BINARY, name="Y_ibto")
 # no se si este bien, solo una idea
 variables_z = []
@@ -69,6 +72,7 @@ model.addConstrs(
   ( Y[i,b,t,o] == Y[i,b-b_p(o,i),t,o] for i in Camiones for o in Origenes for b in Bloques[b_p(o,i):] for t in Dias), 
   name="R1a"
 )
+
 model.addConstrs(
   ( b_p(o,i) <= 47 for i in Camiones for o in Origenes), 
   name="R1b"
@@ -80,25 +84,79 @@ model.addConstrs(
   ( Z[i,b,t,j,d] >= alpha[i,b-b_pp(d,i),t] for i in Camiones for d in Destinos for b in Bloques[b_pp(d,i):] for t in Dias for j in Pedidos(d)), 
   name="R2a"
 )
+
 model.addConstrs(
   ( b_pp(d,i) <= 47 for i in Camiones for d in Destinos), 
   name="R2b"
 )
 
 # R3
+r3a_sum1 = lambda b,t: quicksum(W[i,b,t,o] for o in Origenes for i in Camiones)
+r3a_sum2 = lambda t: quicksum(Md[d,t] for d in Destinos)
+model.addConstrs(
+  ( U[1,t+1] == U[48,t] + r3a_sum1(b,t) - r3a_sum2(t) for b in Bloques for t in Dias[:-1]),
+  name="R3a"
+)
+
+r3b_sum1 = lambda b,t: quicksum(W[i,b,t,o] for o in Origenes for i in Camiones)
+r3b_sum2 = lambda b,t: quicksum(X[i,b,t,d] for d in Destinos for i in Camiones)
+model.addConstrs(
+  ( U[b+1,t] == U[b,t] + r3b_sum1(b,t) - r3b_sum2(b,t) for b in Bloques[:-1] for t in Dias),
+  name="R3b"
+)
+
+r3c_sum1 = quicksum(W[i,1,1,o] for o in Origenes for i in Camiones)
+r3c_sum2 = quicksum(X[i,1,1,d] for d in Destinos for i in Camiones)
+model.addConstr(
+  U[1,1] == r3c_sum1 - r3c_sum2,
+  name="R3c"
+)
 
 # R4
 model.addConstrs(
-  ( U[48,t-1] == U[1,t] for t in Dias[1:]), name="R4"
+  ( U[48,t-1] == U[1,t] for t in Dias[1:] ), name="R4"
 )
 
 # R5
+r5_sum1 = lambda i,b,t: quicksum(Z[i,b,t,j,d] for d in Destinos for j in Pedidos(d))
+r5_sum2 = lambda i,b,t: quicksum(Y[i,b,t,o] for o in Origenes)
+model.addConstrs(
+  ( alpha[i,b,t] + r5_sum1(i,b,t) + r5_sum2(i,b,t) <= 1 for i in Camiones for b in Bloques for t in Dias), 
+  name="R5"
+)
 
 # R6
+# en el word sale para esta restricción que esta pendiente modificarla
+model.addConstrs(
+  ( quicksum(W[i,b,t,o] for i in Camiones for b in Bloques) <= Mo[o,t] for t in Dias for o in Origenes), 
+  name="R6a"
+)
+
+# en la siguiente restricción asumi que la variable x que 
+#  aparece en el word es un error de tipeo de la variable Z
+model.addConstrs(
+  ( quicksum(Z[i,b,t,j,d] for i in Camiones) == Md[d,t] for t in Dias for b in Bloques for d in Destinos for j in Pedidos(d)), 
+  name="R6b"
+)
 
 # R7
+model.addConstrs(
+  ( Z[i,b,t,j,d] <= Q[i] for i in Camiones for t in Dias for b in Bloques for d in Destinos for j in Pedidos(d)), 
+  name="R7a"
+)
+model.addConstrs(
+  ( W[i,b,t,o] <= Q[i] for i in Camiones for t in Dias for b in Bloques for o in Origenes), 
+  name="R7b"
+)
 
 # R8
+r8_sum1 = lambda i,b,t: quicksum(Z[i,b,t,j,d]*Dd[d] for d in Destinos for j in Pedidos(d))
+r8_sum2 = lambda i,b,t: quicksum(Y[i,b,t,o]*Do[o] for o in Origenes)
+r8_sum3 = lambda t,b: quicksum(beta[i]*Cc[i] + 2*(Ckm[i] + E[i]*Ce)*r8_sum1(i,b,t) + r8_sum2(i,b,t) for i in Camiones)
+model.addConstr(
+  quicksum( U[b,t]*Cq + r8_sum3(t,b) for t in Dias for b in Bloques) <= G,
+  name="R8"
+)
 
 # R9
 model.addConstrs(
@@ -106,18 +164,52 @@ model.addConstrs(
 )
 
 # R10
+# la siguiente restricción esta distinta en el word, pero segun yo al convertir la 
+#  sumatoria de destintos en un cuantificador afuera, no altera la restricción. Lo hice para facilitar el código.
+model.addConstrs(
+  ( quicksum(Z[i,b,t,j,d] for i in Camiones for b in Bloques for t in Dias) >= 1 for d in Destinos for j in Pedidos(d)), 
+  name="R10"
+)
 
 # R11
+model.addConstrs(
+  ( Y[i,b,t,o]*(b + Do[o]*(V[i]**-1))  <= tmaxo for i in Camiones for b in Bloques for t in Dias for o in Origenes), 
+  name="R11a"
+)
+
+model.addConstrs(
+  ( Z[i,b,t,j,d]*(b + Dd[d]*(V[i]**-1)) <= tmaxd for i in Camiones for b in Bloques for t in Dias for d in Destinos for j in Pedidos(d)), 
+  name="R11b"
+)
 
 # R12
+model.addConstrs(
+  ( (1 - Z[i,b,t,j,d]) >= alpha[i,b,t] for i in Camiones for b in Bloques for t in Dias for d in Destinos for j in Pedidos(d)), 
+  name="R12a"
+)
+
+model.addConstrs(
+  ( (1 - Y[i,b,t,o]) >= alpha[i,b,t] for i in Camiones for b in Bloques for t in Dias for o in Origenes), 
+  name="R12b"
+)
+
+# quedan pendientes R12c y R12d que no las entendí bien en el word, mas que nada el indice b
 
 # R13
+model.addConstrs(
+  ( quicksum(alpha[i,b,t] for b in Bloques for t in Dias) <= bigM*beta[i] for i in Camiones), 
+  name="R13"
+)
 
-# R14 
+# R14
+model.addConstrs(
+  ( M[b,t,o] == R[o] + M[b,t-1,o] - quicksum(W[i,b,t,o] for i in Camiones) for o in Origenes for b in Bloques for t in Dias[1:]), 
+  name="R14"
+)
+
+# R15 
 # Las restricciones de la naturaleza de las variables las establece gurobi
 #  al crear las variables y definir sus respectivos tipos de datos 
-
-# faltan el resto de las restricciones
 
 # ------------ Función objetivo ------------
 fo_sum1 = lambda t,b,i: quicksum(Z[i,b,t,j,d]*Dd[d] for d in Destinos for j in Pedidos(d))
